5700679f57fef6b70ae380d2045a250f7bb3645e
LourenceQ <lqfmecatronica@gmail.com>
2021-10-14 14:54:03 -0300

    new sql connection

--- GamesRegistrationApi/GamesRegistrationApi/Repository/GameSqlServerRepository.cs
+++ GamesRegistrationApi/GamesRegistrationApi/Repository/GameSqlServerRepository.cs
@@ -11,28 +11,35 @@ namespace GamesRegistrationApi.Repository
     public class GameSqlServerRepository : IGameRepository
     {
         private readonly SqlConnection sqlConnection;
-
         public GameSqlServerRepository(IConfiguration configuration)
         {
             sqlConnection = new SqlConnection(configuration.GetConnectionString("Default"));
         }
         public async Task Delete(Guid id)
         {
-            var con = $"delete from Jogos where Id = '{id}'";
+            var con = $"delete from Game where Id = '{id}'";
 
             await sqlConnection.OpenAsync();
+
             SqlCommand sqlCommand = new SqlCommand(con, sqlConnection);
             sqlCommand.ExecuteNonQuery();
+
             await sqlConnection.CloseAsync();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
         }
 
         public async Task<List<Game>> Get(int page, int quantity)
         {
-            var games  = new List<Game>();
+            var games = new List<Game>();
 
-            var con = $"select * from Jogos order by id offset {((page - 1) * quantity)} rows fetch next {quantity} rows only";
+            var con = $"select * from Game order by id offset {((page - 1) * quantity)} rows fetch next {quantity} rows only";
 
             await sqlConnection.OpenAsync();
+
             SqlCommand sqlCommand = new SqlCommand(con, sqlConnection);
             SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
 
@@ -56,18 +63,18 @@ namespace GamesRegistrationApi.Repository
         {
             var games = new List<Game>();
 
-            var con = $"select * from Jogos where Nome = '{name}' and Produtora = '{producer}'";
+            var con = $"select * from Game where Name = '{name}' and Producer = '{producer}'";
 
             await sqlConnection.OpenAsync();
             SqlCommand sqlCommand = new SqlCommand(con, sqlConnection);
             SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
-
+            
             while (sqlDataReader.Read())
             {
                 games.Add(new Game
                 {
                     Id = (Guid)sqlDataReader["Id"],
-                    Name = (string)sqlDataReader["Name"],
+                    Name = (string)sqlDataReader["Name"], 
                     Producer = (string)sqlDataReader["Producer"],
                     Price = (double)sqlDataReader["Price"]
                 });
@@ -82,9 +89,10 @@ namespace GamesRegistrationApi.Repository
         {
             Game game = null;
 
-            var con = $"select * from Jogos where Id = '{id}'";
+            var con = $"Select * from Game where Id = '{id}'";
 
             await sqlConnection.OpenAsync();
+
             SqlCommand sqlCommand = new SqlCommand(con, sqlConnection);
             SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
 
@@ -106,11 +114,13 @@ namespace GamesRegistrationApi.Repository
 
         public async Task Insert(Game game)
         {
-            var con = $"insert Jogos (Id, Nome, Produtora, Preco) values ('{game.Id}', '{game.Name}', '{game.Producer}', {game.Price.ToString().Replace(",", ".")})";
-
+            var con = $"insert Game (Id, Name, Producer, Price) values ('{game.Id}', '{game.Name}', '{game.Producer}', '{game.Price.ToString().Replace(",", ".")}')";
+            
             await sqlConnection.OpenAsync();
+
             SqlCommand sqlCommand = new SqlCommand(con, sqlConnection);
             sqlCommand.ExecuteNonQuery();
+
             await sqlConnection.CloseAsync();
         }
 
@@ -119,9 +129,11 @@ namespace GamesRegistrationApi.Repository
             var con = $"update Jogos set Name = '{game.Name}', Producer = '{game.Producer}', Price = {game.Price.ToString().Replace(",", ".")} where Id = '{game.Id}'";
 
             await sqlConnection.OpenAsync();
+
             SqlCommand sqlCommand = new SqlCommand(con, sqlConnection);
             sqlCommand.ExecuteNonQuery();
+
             await sqlConnection.CloseAsync();
         }
-    }
+    }    
 }
